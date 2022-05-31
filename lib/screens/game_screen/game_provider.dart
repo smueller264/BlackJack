407cb3e5a01f861f536bdbc086f82c36d412d05a
Sebastian M <sebastian@sebastiansmbp2.speedport.ip>
2022-05-31 18:30:57 +0200

    implemented basic card dealing

--- lib/screens/game_screen/game_provider.dart
+++ lib/screens/game_screen/game_provider.dart
@@ -1,8 +1,28 @@
-import 'package:provider/provider.dart';
 import 'package:flutter/material.dart';
+
+import '../../models/playing_card.dart';
 
 ///Holds the State of the BlackJack game and cointains the game logic
 class GameProvider extends ChangeNotifier {
+  ///Identifies who is currently drawing cards, the Dealer or the Player
+  String currentPlayer = "Player";
   int playerScore = 0;
   int dealerScore = 0;
+
+  List<PlayingCard> playerCards = [];
+  List<PlayingCard> dealerCards = [];
+
+  GameProvider() {
+    dealCard();
+    playerCards.forEach((element) {
+      print(element.type);
+    });
+  }
+
+  void dealCard() {
+    for (int i = 0; i < 20; i++) {
+      //playerCards.add(PlayingCard.random());
+      playerCards.add(PlayingCard.random());
+    }
+  }
 }
